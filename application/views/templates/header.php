7de870e40a2c768ec4bb01bf126b523c9b750ab1
lanz <lanz.blank19@gmail.com>
2018-06-06 15:55:06 +0800

    header background change

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,7 +2,7 @@
         <head>
                 <title>CodeIgniter Tutorial</title>
         </head>
-        <body bgcolor="navy"><center><br><br><br><br><br><br>
+        <body bgcolor="pink"><center><br><br><br><br><br><br>
  
                 <span style="color:red;font-size:150px;font-style:italic;font-weight:bold;font-family:'Times New Roman', Times, serif;">COOL</span>
              <p><a href="<?php echo site_url('news'); ?>">Home</a> | <a href="<?php echo site_url('news/create'); ?>">Just Add</a></p>
